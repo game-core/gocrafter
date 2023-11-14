4de00dc3ce666718caf118ac131fabe7d06b96d9
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-11-14 22:25:00 +0900

    Accountを追加

---
+++ docs/sql/user/account.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE account (
+
+	id BIGINT NOT NULL AUTO_INCREMENT,
+
+	uuid BIGINT NOT NULL ,
+
+	name VARCHAR(255) NOT NULL ,
+
+	password VARCHAR(255) NOT NULL ,
+
+	created_at TIMESTAMP NOT NULL ,
+
+	updated_at TIMESTAMP NOT NULL ,
+
+	PRIMARY KEY(id),
+	INDEX(uuid)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
