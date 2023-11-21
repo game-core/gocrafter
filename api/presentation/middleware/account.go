143af11793e2c26ff8e25ae7ca57193952c49257
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-11-21 17:52:56 +0900

    middlewearのエラーハンドリングを修正

--- api/presentation/middleware/account.go
+++ api/presentation/middleware/account.go
@@ -6,6 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"os"
 	"strings"
+	"time"
 )
 
 type AccountMiddleware interface {
@@ -21,22 +22,47 @@ func NewAccountMiddleware() AccountMiddleware {
 // AccountMiddleware トークンを検証する
 func (a *accountMiddleware) AccountMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		tokenString := strings.ReplaceAll(c.Request().Header.Get("Authorization"), "Bearer ", "")
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-			}
-
-			return []byte(os.Getenv("AUTH_SECRET")), nil
-		})
+		tokenString, err := extractTokenFromHeader(c)
 		if err != nil {
 			return err
 		}
 
-		if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		token, err := parseToken(tokenString)
+		if err != nil {
+			return err
+		}
+
+		if isValidToken(token) {
 			return next(c)
 		}
 
 		return fmt.Errorf("Invalid token")
 	}
+}
+
+func extractTokenFromHeader(c echo.Context) (string, error) {
+	tokenString := strings.ReplaceAll(c.Request().Header.Get("Authorization"), "Bearer ", "")
+	if tokenString == "" {
+		return "", fmt.Errorf("Authorization header is missing")
+	}
+
+	return tokenString, nil
+}
+
+func parseToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if signingMethod, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		} else if signingMethod != jwt.SigningMethodHS256 {
+			return nil, fmt.Errorf("unexpected signing method: %v", signingMethod.Alg())
+		}
+
+		return []byte(os.Getenv("AUTH_SECRET")), nil
+	})
+}
+
+func isValidToken(token *jwt.Token) bool {
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	return ok && token.Valid && claims.VerifyExpiresAt(time.Now().Unix(), true)
 }
