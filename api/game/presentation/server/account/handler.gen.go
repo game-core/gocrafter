f7ecd5741113451994cbd70cb10fe7418d18d7fa
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2024-02-24 04:09:59 +0900

    Update handler.gen.go

--- api/game/presentation/server/account/handler.gen.go
+++ api/game/presentation/server/account/handler.gen.go
@@ -1,1 +1,35 @@
 package account
+
+import (
+	"context"
+	"fmt"
+
+	accountUsecase "github.com/game-core/gocrafter/api/game/usecase/account"
+)
+
+type AccountHandler interface {
+	AccountServer
+}
+
+type accountHandler struct {
+	UnimplementedAccountServer
+	accountUsecase accountUsecase.AccountUsecase
+}
+
+func NewAccountHandler(
+	accountUsecase accountUsecase.AccountUsecase,
+) AccountHandler {
+	return &accountHandler{
+		accountUsecase: accountUsecase,
+	}
+}
+
+// Create アカウントを作成する
+func (s *accountHandler) Create(ctx context.Context, req *AccountCreateRequest) (*AccountCreateResponse, error) {
+	res, err := s.accountUsecase.Create(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("s.accountUsecase.Create: %s", err)
+	}
+
+	return res, nil
+}
