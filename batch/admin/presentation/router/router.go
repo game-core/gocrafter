fe9434eae43dd04b3f0712144b68e3191a1b537a
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2024-02-24 02:19:52 +0900

    Create router.go

---
+++ batch/admin/presentation/router/router.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"flag"
+	"log"
+
+	"github.com/game-core/gocrafter/pkg/logger"
+)
+
+func Router() {
+	// Batch list
+	batchList := map[string]func() error{}
+
+	commandPtr := flag.String("command", "", "Specify the command")
+	flag.Parse()
+	command := *commandPtr
+	selectedCommand := command
+
+	// Run
+	if batch, ok := batchList[selectedCommand]; ok {
+		logFile := logger.GenerateBatchGameDebug()
+		log.SetOutput(logFile)
+		if err := batch(); err != nil {
+			log.Printf("failed to batch(): %s", err)
+		}
+	}
+}
