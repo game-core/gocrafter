a1ab932691629be14560647898038beabc290d7d
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2024-02-24 18:39:52 +0900

    Update errors.go

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -5,7 +5,12 @@ import (
 )
 
 // NewError エラー
-func NewError(method string, err error) error {
+func NewError(message string) error {
+	return fmt.Errorf(message)
+}
+
+// NewMethodError エラー
+func NewMethodError(method string, err error) error {
 	return fmt.Errorf("failed to %s: %s", method, err)
 }
 
