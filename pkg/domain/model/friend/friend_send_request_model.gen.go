19f2844cc2ff9111d1ddb3e165bdd705e0682c74
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2024-02-29 15:19:42 +0900

    Add: user friend domain

---
+++ pkg/domain/model/friend/friend_send_request_model.gen.go
@@ -0,0 +1,24 @@
+// Package friend フレンド申請リクエスト
+package friend
+
+type FriendSendRequests []*FriendSendRequest
+
+type FriendSendRequest struct {
+	UserId       string
+	FriendUserId string
+}
+
+func NewFriendSendRequest() *FriendSendRequest {
+	return &FriendSendRequest{}
+}
+
+func NewFriendSendRequests() FriendSendRequests {
+	return FriendSendRequests{}
+}
+
+func SetFriendSendRequest(userId string, friendUserId string) *FriendSendRequest {
+	return &FriendSendRequest{
+		UserId:       userId,
+		FriendUserId: friendUserId,
+	}
+}
---
+++ pkg/domain/model/friend/friend_send_response_model.gen.go
@@ -0,0 +1,26 @@
+// Package friend フレンド申請レスポンス
+package friend
+
+import (
+	"github.com/game-core/gocrafter/pkg/domain/model/friend/userFriend"
+)
+
+type FriendSendResponses []*FriendSendResponse
+
+type FriendSendResponse struct {
+	UserFriend *userFriend.UserFriend
+}
+
+func NewFriendSendResponse() *FriendSendResponse {
+	return &FriendSendResponse{}
+}
+
+func NewFriendSendResponses() FriendSendResponses {
+	return FriendSendResponses{}
+}
+
+func SetFriendSendResponse(userFriend *userFriend.UserFriend) *FriendSendResponse {
+	return &FriendSendResponse{
+		UserFriend: userFriend,
+	}
+}
