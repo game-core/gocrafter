67169e5776f566cd48c56318e4f13d554a152e35
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2024-03-17 16:44:28 +0900

    Add: Resource domain

--- pkg/domain/model/resource/resource_service.go
+++ pkg/domain/model/resource/resource_service.go
@@ -2,10 +2,16 @@
 package resource
 
 import (
+	"context"
+
+	"github.com/game-core/gocrafter/internal/errors"
+	"github.com/game-core/gocrafter/pkg/domain/enum"
 	"github.com/game-core/gocrafter/pkg/domain/model/resource/masterResource"
 )
 
 type ResourceService interface {
+	GetAll(cxt context.Context) (masterResource.MasterResources, error)
+	GetByResourceType(cxt context.Context, resourceType enum.ResourceType) (*masterResource.MasterResource, error)
 }
 
 type resourceService struct {
@@ -18,4 +24,24 @@ func NewResourceService(
 	return &resourceService{
 		masterResourceRepository: masterResourceRepository,
 	}
+}
+
+// GetAll リソース一覧を取得する
+func (s *resourceService) GetAll(cxt context.Context) (masterResource.MasterResources, error) {
+	results, err := s.masterResourceRepository.FindList(cxt)
+	if err != nil {
+		return nil, errors.NewMethodError("s.masterResourceRepository.FindList", err)
+	}
+
+	return results, nil
+}
+
+// GetByResourceType リソースを取得する
+func (s *resourceService) GetByResourceType(cxt context.Context, resourceType enum.ResourceType) (*masterResource.MasterResource, error) {
+	result, err := s.masterResourceRepository.FindByResourceType(cxt, resourceType)
+	if err != nil {
+		return nil, errors.NewMethodError("s.masterResourceRepository.FindByResourceType", err)
+	}
+
+	return result, nil
 }
--- pkg/domain/model/resource/resource_service_test.go
+++ pkg/domain/model/resource/resource_service_test.go
@@ -1,9 +1,14 @@
 package resource
 
 import (
+	"context"
 	"reflect"
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	
+	"github.com/game-core/gocrafter/internal/errors"
+	"github.com/game-core/gocrafter/pkg/domain/enum"
 	"github.com/game-core/gocrafter/pkg/domain/model/resource/masterResource"
 )
 
@@ -33,6 +38,227 @@ func TestNewResourceService_NewResourceService(t *testing.T) {
 			)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewResourceService() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestResourceService_GetAll(t *testing.T) {
+	type fields struct {
+		masterResourceRepository func(ctrl *gomock.Controller) masterResource.MasterResourceRepository
+	}
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    masterResource.MasterResources
+		wantErr error
+	}{
+		{
+			name: "正常：取得できる場合",
+			fields: fields{
+				masterResourceRepository: func(ctrl *gomock.Controller) masterResource.MasterResourceRepository {
+					m := masterResource.NewMockMasterResourceRepository(ctrl)
+					m.EXPECT().
+						FindList(
+							nil,
+						).
+						Return(
+							masterResource.MasterResources{
+								{
+									Id:           1,
+									Name:         "ノーマル",
+									ResourceType: enum.ResourceType_Normal,
+								},
+								{
+									Id:           2,
+									Name:         "カード",
+									ResourceType: enum.ResourceType_Card,
+								},
+								{
+									Id:           3,
+									Name:         "チケット",
+									ResourceType: enum.ResourceType_Ticket,
+								},
+								{
+									Id:           4,
+									Name:         "コイン",
+									ResourceType: enum.ResourceType_Coin,
+								},
+								{
+									Id:           5,
+									Name:         "クリスタル",
+									ResourceType: enum.ResourceType_Crystal,
+								},
+							},
+							nil,
+						)
+					return m
+				},
+			},
+			args: args{
+				ctx: nil,
+			},
+			want: masterResource.MasterResources{
+				{
+					Id:           1,
+					Name:         "ノーマル",
+					ResourceType: enum.ResourceType_Normal,
+				},
+				{
+					Id:           2,
+					Name:         "カード",
+					ResourceType: enum.ResourceType_Card,
+				},
+				{
+					Id:           3,
+					Name:         "チケット",
+					ResourceType: enum.ResourceType_Ticket,
+				},
+				{
+					Id:           4,
+					Name:         "コイン",
+					ResourceType: enum.ResourceType_Coin,
+				},
+				{
+					Id:           5,
+					Name:         "クリスタル",
+					ResourceType: enum.ResourceType_Crystal,
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "異常：s.masterResourceRepository.FindList",
+			fields: fields{
+				masterResourceRepository: func(ctrl *gomock.Controller) masterResource.MasterResourceRepository {
+					m := masterResource.NewMockMasterResourceRepository(ctrl)
+					m.EXPECT().
+						FindList(
+							nil,
+						).
+						Return(
+							nil,
+							errors.NewTestError(),
+						)
+					return m
+				},
+			},
+			args: args{
+				ctx: nil,
+			},
+			want:    nil,
+			wantErr: errors.NewMethodError("s.masterResourceRepository.FindList", errors.NewTestError()),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			s := &resourceService{
+				masterResourceRepository: tt.fields.masterResourceRepository(ctrl),
+			}
+
+			got, err := s.GetAll(tt.args.ctx)
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("GetAll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetAll() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestResourceService_GetByResourceType(t *testing.T) {
+	type fields struct {
+		masterResourceRepository func(ctrl *gomock.Controller) masterResource.MasterResourceRepository
+	}
+	type args struct {
+		ctx          context.Context
+		resourceType enum.ResourceType
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *masterResource.MasterResource
+		wantErr error
+	}{
+		{
+			name: "正常：取得できる場合",
+			fields: fields{
+				masterResourceRepository: func(ctrl *gomock.Controller) masterResource.MasterResourceRepository {
+					m := masterResource.NewMockMasterResourceRepository(ctrl)
+					m.EXPECT().
+						FindByResourceType(
+							nil,
+							enum.ResourceType_Normal,
+						).
+						Return(
+							&masterResource.MasterResource{
+								Id:           1,
+								Name:         "ノーマル",
+								ResourceType: enum.ResourceType_Normal,
+							},
+							nil,
+						)
+					return m
+				},
+			},
+			args: args{
+				ctx: nil,
+			},
+			want: &masterResource.MasterResource{
+				Id:           1,
+				Name:         "ノーマル",
+				ResourceType: enum.ResourceType_Normal,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "異常：s.masterResourceRepository.FindByResourceType",
+			fields: fields{
+				masterResourceRepository: func(ctrl *gomock.Controller) masterResource.MasterResourceRepository {
+					m := masterResource.NewMockMasterResourceRepository(ctrl)
+					m.EXPECT().
+						FindByResourceType(
+							nil,
+							enum.ResourceType_Normal,
+						).
+						Return(
+							nil,
+							errors.NewTestError(),
+						)
+					return m
+				},
+			},
+			args: args{
+				ctx: nil,
+			},
+			want:    nil,
+			wantErr: errors.NewMethodError("s.masterResourceRepository.FindByResourceType", errors.NewTestError()),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			s := &resourceService{
+				masterResourceRepository: tt.fields.masterResourceRepository(ctrl),
+			}
+
+			got, err := s.GetByResourceType(tt.args.ctx, tt.args.resourceType)
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("GetByResourceType() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetByResourceType() = %v, want %v", got, tt.want)
 			}
 		})
 	}
