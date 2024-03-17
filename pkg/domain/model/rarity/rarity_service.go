b6c85bb8fe041085993f3b7add2ca286a7ece4b6
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2024-03-17 16:40:31 +0900

    Add: Rarity domain

--- pkg/domain/model/rarity/rarity_service.go
+++ pkg/domain/model/rarity/rarity_service.go
@@ -2,10 +2,16 @@
 package rarity
 
 import (
+	"context"
+
+	"github.com/game-core/gocrafter/internal/errors"
+	"github.com/game-core/gocrafter/pkg/domain/enum"
 	"github.com/game-core/gocrafter/pkg/domain/model/rarity/masterRarity"
 )
 
 type RarityService interface {
+	GetAll(cxt context.Context) (masterRarity.MasterRarities, error)
+	GetByRarityType(cxt context.Context, rarityType enum.RarityType) (*masterRarity.MasterRarity, error)
 }
 
 type rarityService struct {
@@ -18,4 +24,24 @@ func NewRarityService(
 	return &rarityService{
 		masterRarityRepository: masterRarityRepository,
 	}
+}
+
+// GetAll レアリティ一覧を取得する
+func (s *rarityService) GetAll(cxt context.Context) (masterRarity.MasterRarities, error) {
+	results, err := s.masterRarityRepository.FindList(cxt)
+	if err != nil {
+		return nil, errors.NewMethodError("s.masterRarityRepository.FindList", err)
+	}
+
+	return results, nil
+}
+
+// GetByRarityType レアリティを取得する
+func (s *rarityService) GetByRarityType(cxt context.Context, rarityType enum.RarityType) (*masterRarity.MasterRarity, error) {
+	result, err := s.masterRarityRepository.FindByRarityType(cxt, rarityType)
+	if err != nil {
+		return nil, errors.NewMethodError("s.masterRarityRepository.FindByRarityType", err)
+	}
+
+	return result, nil
 }
